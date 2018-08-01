73e784720e7896aaa6c1760d89cf445181966d1a
yushulx <lingxiao1002@gmail.com>
2018-08-02 06:14:23 +0800

    Fixed module error

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-// const Module = require('./test.js');
-// const wasm = Module({wasmBinaryFile: 'test.wasm'});
-// wasm.onRuntimeInitialized = function() {
-//     console.log(wasm._add(40, 40));
-// };
-
 const fs = require('fs');
 var source = fs.readFileSync('test.jpg');
 var typedArray = new Uint8Array(source);
-const Module = require('./dbr.min.js');
+const Module = require('dbrjs');
 Module.onRuntimeInitialized = function() {
     let dbr = new Module.BarcodeReaderWasm("t0068NQAAAKTSQDbEid8CTEeNluhTXi+h35G8R03xIHsyYNzZoa2GiU2a8y7s5Z1lfHsMW5dNyZmH6jQL51HUcoB5EhpDeDk=");
     console.time('wasm');
